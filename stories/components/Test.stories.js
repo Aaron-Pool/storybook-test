80a3faa3c8a65576ccff3e0969d1f290d82dc263
Aaron Pool <aaron.pool@cadre5.com>
2019-11-07 20:52:28 -0500

    fixed example

--- stories/components/Test.stories.js
+++ stories/components/Test.stories.js
@@ -1,13 +1,13 @@
 import Vuex from 'vuex'
 import { storiesOf } from '@storybook/vue'
 import Test from '../../components/Test'
-import { state } from '../../store/storetest'
+import * as storetest from '../../store/storetest'
 
 storiesOf(`Test`, module)
   .add(
     `Test`,
     () => ({
-      store: new Vuex.Store({ state: state() }),
+      store: new Vuex.Store({ modules: { storetest } }),
       components: {
         Test
       },
